40cd14699eb914ec1863fe2e47972d945f7a22d5
Wasurat_L <60914696+Aum2010@users.noreply.github.com>
2021-01-16 13:31:15 +0700

    merge printer functions in this file

--- addgps.js
+++ addgps.js
@@ -9,7 +9,8 @@ const sys = require('util');
 var mqtt = require('mqtt');
 
 app.use(cors())
-app.use(bodyParser.urlencoded({extended: true }))
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb',extended: true }))
 const exec = util.promisify(require('child_process').exec);
 
 //*********************** MQTT Config ****************************//
@@ -152,4 +153,31 @@ client.on('message', (topic, message) => {
 
 });
 // Lat Lng Steam 
+
+// Printer Function
+async function lsWithGrep() {
+  try {
+      //await exec('sudo qrcode -o out.png ${msg}');
+	//await QR.toFile("out.png",msg);
+      const { stdout, stderr } = await exec('sudo lp -o fit-to-page image.png');
+      console.log('stdout:', stdout);
+      console.log('stderr:', stderr);
+  }catch {
+     return ;
+  };
+};
+
+app.post('/print',(req,res) => {
+	// Remove header
+	//console.log(req.body.data)
+	let base64Image = req.body.data.toString().split(';base64,').pop();
+	fs.writeFile('image.png', base64Image, {encoding: 'base64'}, function(err) {
+	  console.log('File created');
+	});
+//	lsWithGrep();  //<-- Enable Printer
+	res.json({"msg":"1"})
+	res.status(200)
+})
+// Printer Function
+
 
